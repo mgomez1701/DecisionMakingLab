7041f16dbb676c6a7eb436419aa52349db4b9f1c
Mike <michael005@me.com>
2019-10-15 15:41:11 -0400

    Creating Decision Making Lab Repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecisionMakingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
